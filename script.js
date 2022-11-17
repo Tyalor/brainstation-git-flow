2b014a8b54751e673e8cd057dcbf65daf5bb54ac
Tlau425 <tlau425@yahoo.com>
2022-11-17 16:18:39 -0500

    added new object and changed background color

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ const poems = [
   },
 
   // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
+  {
+    author: "Tommy",
+    poem: "Roses are red \n pizza is good",
+    image: "pizzaNew.jpeg"
+  }
 ];
 
 let displayPoems = (array) => {
